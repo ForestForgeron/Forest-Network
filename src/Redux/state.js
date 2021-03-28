1ad448b1467e29022db04a3e00567dbf60d8a218
forestforgeron <forestforgeron@gmail.com>
2021-03-28 17:56:39 +0300

    state added. logic was moved from components into state

---
+++ src/Redux/state.js
@@ -0,0 +1,87 @@
+let state = {
+  dialoguesPage: {
+    conversationsList: [
+      {
+        id: 1,
+        name: 'Olga',
+      },
+    
+      {
+        id: 2,
+        name: 'Andrey',
+      },
+    
+      {
+        id: 3,
+        name: 'Gena',
+      },
+    
+      {
+        id: 4,
+        name: 'Ilya',
+      },
+    
+      {
+        id: 5,
+        name: 'Victor',
+      },
+    
+    ],
+
+    messagesList: [
+      {
+        id: 1,
+        message: 'Hi! How are you?',
+      },
+    
+      {
+        id: 2,
+        message: 'Nice weather today!',
+      },
+    
+      {
+        id: 3,
+        message: 'Might and Magic',
+      },
+    
+      {
+        id: 4,
+        message: 'yo',
+      },
+    
+      {
+        id: 5,
+        message: 'me gusto',
+      },
+    
+    ],
+
+  },
+
+  ProfilePage: {
+    postsList: [
+      {
+        id: 1,
+        post: 'Hello world!',
+        likesCount: 15,
+      },
+    
+      {
+        id: 2,
+        post: 'Feel the power of the youth!',
+        likesCount: 32,
+      },
+    
+      {
+        id: 3,
+        post: 'Dabu-di-dabu-dai',
+        likesCount: 10,
+      },
+    
+    ],
+  },
+
+
+};
+
+export default state;
